dcb091ac78d0b31a51c50722c0366df14dd50cb5
Nur Halim <44104311+nurhalimofficial@users.noreply.github.com>
2022-03-21 21:48:16 +0700

    Aplication

---
+++ application/controllers/User.php
@@ -0,0 +1,112 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class User extends CI_Controller {
+
+    function __construct()
+    {
+        parent::__construct();
+        check_not_login();
+        check_admin();
+        $this->load->model('user_m');
+        $this->load->library('form_validation');
+    }
+
+	public function index()
+	{
+        $data ['row']= $this->user_m->get();
+
+        $this->template->load('template', 'user/user_data', $data);
+    }
+    
+    public function add()
+    {
+        $this->form_validation->set_rules('fullname', 'Nama', 'required');
+        $this->form_validation->set_rules('username', 'Username', 'required|min_length[5]|is_unique[user.username]');
+        $this->form_validation->set_rules('password', 'Password', 'required|min_length[5]');
+        $this->form_validation->set_rules('passconf', 'Konfirmasi Password', 'required|min_length[5]|matches[password]',
+            array('matches' => '%s tidak sesuai')
+        );
+        $this->form_validation->set_rules('address', 'Alamat', 'required');
+        $this->form_validation->set_rules('level', 'Level', 'required');
+
+        $this->form_validation->set_message('required', '%s masih kosong, silahkan isi');
+        $this->form_validation->set_message('min_length', '{field} minimal 5 karakter');
+        $this->form_validation->set_message('is_unique', '{field} ini sudah terpakai, silahkan ganti');
+
+        if ($this->form_validation->run() == FALSE) {
+            $this->template->load('template', 'user/user_form_add');
+        } else {
+            $post = $this->input->post(null, TRUE);
+            $this->user_m->add($post);
+            if($this->db->affected_rows() > 0) {
+                echo "<script>alert('Data berhasil disimpan');</script>";
+            }
+            echo "<script>window.location='".site_url('user')."';</script>";
+        }
+  
+    }
+
+    public function edit($id)
+    {
+        $this->form_validation->set_rules('fullname', 'Nama', 'required');
+        $this->form_validation->set_rules('username', 'Username', 'required|min_length[5]|callback_username_check');
+        if($this->input->post('password')) {
+            $this->form_validation->set_rules('password', 'Password', 'required|min_length[5]');
+            $this->form_validation->set_rules('passconf', 'Konfirmasi Password', '|min_length[5]|matches[password]',
+                array('matches' => '%s tidak sesuai')
+            );
+        }
+        if($this->input->post('passconf')) {
+            $this->form_validation->set_rules('passconf', 'Konfirmasi Password', '|min_length[5]|matches[password]',
+                array('matches' => '%s ora podo passwordte')
+            );
+        }
+        $this->form_validation->set_rules('address', 'Alamat', 'required');
+        $this->form_validation->set_rules('level', 'Level', 'required');
+
+        $this->form_validation->set_message('required', '%s isih kosong, monggo isi');
+        $this->form_validation->set_message('min_length', '{field} minimal 5 karakter');
+        $this->form_validation->set_message('is_unique', '{field} ini wes di enggo, diganti yo');
+
+        if ($this->form_validation->run() == FALSE) {
+            $query = $this->user_m->get($id);
+            if($query->num_rows() > 0) {
+                $data['row'] = $query->row();
+                $this->template->load('template', 'user/user_form_edit', $data);
+            } else {
+                echo "<script>alert('Data ora ditemokne');";
+                echo "window.location='".site_url('user')."';</script>";
+            }
+        } else {
+            $post = $this->input->post(null, TRUE);
+            $this->user_m->edit($post);
+            if($this->db->affected_rows() > 0) {
+                echo "<script>alert('Data wes di simpen');</script>";
+            }
+            echo "<script>window.location='".site_url('user')."';</script>";
+        }
+    }
+    function username_check() {
+        $post = $this->input->post(null, TRUE);
+        $query = $this->db->query("SELECT * FROM user WHERE username = '$post[username]' AND user_id != '$post[user_id]'");
+        if($query->num_rows() > 0) {
+            $this->form_validation->set_message('username_check', '{field} Iki wes dienggo, monggo diganti');
+            return FALSE;
+        } else {
+            return TRUE;
+        }
+
+    }
+
+    public function del($id)
+    {
+        $id = $this->input->post('user_id');
+        $this->user_m->del($id);
+
+        if($this->db->affected_rows() > 0) {
+            echo "<script>alert('Data wes dihapus');</script>";
+        }
+        echo "<script>window.location='".site_url('user')."';</script>";
+    }
+
+}
---
+++ application/views/product/category/category_form.php
@@ -0,0 +1,44 @@
+<section class="content-header">
+      <h1>Categories
+        <small>Kategori Barang</small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i></a></li>
+        <li class="active">Categories</li>
+      </ol>
+</section>
+
+<!-- Main content -->
+<section class="content">
+          
+    <div class="box">
+        <div class="box-header">
+            <h3 class="box-title"><?=ucfirst($page)?> category</h3>
+            <div class="pull-right">
+                <a href="<?=site_url('category')?>" class="btn btn-warning btn-flat">
+                    <i class="fa fa-undo"></i> Back
+                </a>
+            </div>
+        </div>
+        <div class="box-body">
+            <div class="col-md-4 col-md-offset-4">
+                <?php // echo validation_errors(); ?>
+                <form action="<?=site_url('category/process')?>" method="post">
+                    <div class="form-group">
+                        <label>Category Name *</label>
+                        <input type="hidden" name="id" value="<?=$row->category_id?>">
+                        <input type="text" name="category_name" value="<?=$row->name?>" class="form-control" required>
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" name="<?=$page?>" class="btn btn-success btn-falt">
+                            <i class="fa  fa-save"></i> Save
+                        </button>
+                        <button type="Reset" class="btn btn-flat"> Reset
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+</section>
